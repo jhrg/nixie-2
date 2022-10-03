51b4c4d91e474b66526279167f0075b3f2164e3b
James Gallagher <jgallagher@opendap.org>
2022-10-03 00:09:08 -0600

    Now uses port registers for faster I/O

--- src/four_digits.cc
+++ src/four_digits.cc
@@ -8,42 +8,43 @@
 
 #define BAUD_RATE 9600
 
+// This is PORTD (bits 7 to 4)
 #define BCD_A PIN4
 #define BCD_B PIN5
 #define BCD_C PIN6
 #define BCD_D PIN7
 
-#define DIGIT_1 14
-#define DIGIT_2 15
+// PORTC (bits 0 and 1; PORTC has only bits 0 - 5)
+#define DIGIT_1 0
+#define DIGIT_2 1
 
-struct bcd_code
+// BCD codes for the digits 0 to 9
+uint8_t bcd[10] = {
+    B00000000,
+    B00010000,
+    B00100000,
+    B00110000,
+    B01000000,
+    B01010000,
+    B01100000,
+    B01110000,
+    B10000000,
+    B10010000};
+
+void display_digit(int value, int digit)
 {
-    int d;
-    int c;
-    int b;
-    int a;
-} codes[10];
+    uint8_t port_d_low_nyble = PORTD & B00001111;
+    PORTD = bcd[value] | port_d_low_nyble;
 
-void display_digit(struct bcd_code code, int digit)
-{
-    digitalWrite(BCD_A, code.a);
-    digitalWrite(BCD_B, code.b);
-    digitalWrite(BCD_C, code.c);
-    digitalWrite(BCD_D, code.d);
-
-    digitalWrite(digit, HIGH);
+    // Only one can be on at any given time. FIXME
+    PORTC |= B0000001 << digit;
+    // digitalWrite(digit, HIGH);
 }
 
-void blank_digit(int digit)
+void blank_display()
 {
-    digitalWrite(digit, LOW);
-#if 0
-    // Only do this with the drivers that have zener diode protection
-    digitalWrite(BCD_A, HIGH);
-    digitalWrite(BCD_B, HIGH);
-    digitalWrite(BCD_C, HIGH);
-    digitalWrite(BCD_D, HIGH);
-#endif
+    PORTC &= B00000000;
+    // digitalWrite(digit, LOW);
 }
 
 void setup()
@@ -52,22 +53,10 @@ void setup()
     Serial.begin(BAUD_RATE);
     Serial.println("boot");
 
-    pinMode(BCD_A, OUTPUT);
-    pinMode(BCD_B, OUTPUT);
-    pinMode(BCD_C, OUTPUT);
-    pinMode(BCD_D, OUTPUT);
-
-    // D C B A
-    codes[0] = {LOW, LOW, LOW, LOW};
-    codes[1] = {LOW, LOW, LOW, HIGH};
-    codes[2] = {LOW, LOW, HIGH, LOW};
-    codes[3] = {LOW, LOW, HIGH, HIGH};
-    codes[4] = {LOW, HIGH, LOW, LOW};
-    codes[5] = {LOW, HIGH, LOW, HIGH};
-    codes[6] = {LOW, HIGH, HIGH, LOW};
-    codes[7] = {LOW, HIGH, HIGH, HIGH};
-    codes[8] = {HIGH, LOW, LOW, LOW};
-    codes[9] = {HIGH, LOW, LOW, HIGH};
+    // Initialize all I/O pins to output
+    DDRD = B11111111;
+    DDRC = B00111111;
+    DDRB = B00111111;
 }
 
 void loop()
@@ -76,14 +65,14 @@ void loop()
     {
         for (int i = 0; i < 100; ++i)
         {
-            display_digit(codes[n % 10], DIGIT_1);
+            display_digit(n % 10 /*codes[n % 10]*/, DIGIT_1);
             delayMicroseconds(1466);
-            blank_digit(DIGIT_1);
+            blank_display();
             delayMicroseconds(200);
 
-            display_digit(codes[n / 10], DIGIT_2);
+            display_digit(n / 10 /*codes[n / 10]*/, DIGIT_2);
             delayMicroseconds(1466);
-            blank_digit(DIGIT_2);
+            blank_display();
             delayMicroseconds(200);
 
             // simulate time for other four digits
