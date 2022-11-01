7a22988194323181d6b79848ea03356d5b39f05e
James Gallagher <jgallagher@opendap.org>
2022-10-31 23:28:37 -0600

    Added state enums

--- include/mode_switch.h
+++ include/mode_switch.h
@@ -3,3 +3,22 @@
 
 void timed_mode_switch_push();
 void timed_mode_switch_release();
+
+enum main_mode
+{
+    show_time,
+    show_weather,
+    set_time
+};
+
+enum set_time_mode
+{
+    set_hours,
+    adv_hours_slow, // no fast advance for hours
+
+    set_minutes,
+    adv_minutes_slow,
+    adv_minutes_fast,
+
+    zero_seconds
+};
