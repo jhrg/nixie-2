7c1e4ec3820153c36d749cac1cbb69e3dee0fce6
James Gallagher <jgallagher@opendap.org>
2022-11-20 11:24:33 -0700

    Added new print code files

---
+++ include/print.h
@@ -0,0 +1,18 @@
+
+/**
+ * A version of printf for the Arduino.
+ * 11/20/22
+ * James Gallagher <jhrg@mac.com>
+ */
+
+void print(const char *fmt, ...);
+void flush();
+
+// Using F() in this macro reduced RAM use from 67% to 50% in ~1200 LOC
+#if DEBUG
+#define DPRINTV(fmt, ...) print((const char *)F(fmt), __VA_ARGS__)
+#define DPRINT(fmt) print((const char *)F(fmt))
+#else
+#define DPRINTV(fmt, ...)
+#define DPRINT(fmt)
+#endif
